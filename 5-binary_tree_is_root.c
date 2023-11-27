8e61d2a8581824a73bf55b60749c205c646fb366
RevolverSnake412 <oussag@hotmail.fr>
2023-11-27 17:54:06 +0000

    Verifies if a node is root or no

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - Verifies if a node is root.
+ * @node: the node in question.
+ *
+ * Return: 0 if node is NULL or not a root node, otherwise 1
+*/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node->parent != NULL || node == NULL)
+		return (0);
+	else
+		return (1);
+}
