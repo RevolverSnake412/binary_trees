7d50c6ba761db6dbd5ff1aa372d513b00a6d59f9
RevolverSnake412 <oussag@hotmail.fr>
2023-11-27 16:24:54 +0000

    Deletes whole binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_tree_delete - Deletes a whole binary tree.
+ * @tree: the target.
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *root = tree;
+
+	if (!root)
+		return;
+
+	if (root->left != NULL)
+		binary_tree_delete(root->left);
+
+	if (root->right != NULL)
+		binary_tree_delete(root->right);
+
+	free(root);
+}
