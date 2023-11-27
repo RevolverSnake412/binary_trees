7f535fff1e375cc2ba1d4c898679175dec5d532b
RevolverSnake412 <oussag@hotmail.fr>
2023-11-27 18:15:51 +0000

    made it fancier

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,15 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_inorder - uses in-order traversal concept.
+ * @tree: start point of a tree.
+ * @func: function to be used (accepts only values of nodes)
  *
+ * Description:
+ * This function goes through a binary tree and calls
+ * a function for each node until the whole tree has
+ * being covered, it will terminate automatically at
+ * the end. (function must only have one parameter).
 */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
@@ -14,5 +22,5 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	func(tree->n);
 
 	if (tree->right != NULL)
-                binary_tree_inorder(tree->right, func);
+		binary_tree_inorder(tree->right, func);
 }
