79d4fcaf1d3bf85529839077dc60f2121fdf11e9
RevolverSnake412 <oussag@hotmail.fr>
2023-11-29 17:38:46 +0000

    update

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,11 +11,11 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	if (node->parent->left->n == node->n)
-        return node->parent->right ? node->parent->right : NULL;
+	if (node->parent->left == node)
+		return (node->parent->right ? node->parent->right : NULL);
 
-	if (node->parent->right->n == node->n)
-		return node->parent->left ? node->parent->left : NULL;
+	if (node->parent->right == node)
+		return (node->parent->left ? node->parent->left : NULL);
 
 	return (NULL);
 }
